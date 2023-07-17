b6bca5fb244f139cfc2fa91f3f856878dcd8cc25
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2023-07-17 16:05:02 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
       {/* //footer section */}
       <div className="mt-3">
         <p className="text-secondary text-center">
-          copyright © 2023 Chayanin Suatap 12345678
+          copyright © 2023 paweennut prohsoontorn 650610783
         </p>
       </div>
     </div>
